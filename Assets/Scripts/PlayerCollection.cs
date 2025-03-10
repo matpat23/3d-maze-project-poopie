74afd2536c254742d52dcc721d5c110d09cda13d
matpat23 <poposkam0@gmail.com>
2025-03-10 13:57:37 +0100

    added score
    i almost shot myself doing this im a visual learner

--- Assets/Scripts/PlayerCollection.cs
+++ Assets/Scripts/PlayerCollection.cs
@@ -1,10 +1,28 @@
 using UnityEngine;
-
+using TMPro;
 public class PlayerCollection : MonoBehaviour
 {
-    // hw; add a scoring system here
+    //add scoring system for hw
+    public int points;
+    public TMP_Text scoreText;
+    public void Score(int value)
+
+    {
+        points = points + value;
+        scoreText.text = $"<b>Score</b> {points}";
+
+
+    }
+    
+
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(other.gameObject);
+        //only destroy if collectable
+        if (other.CompareTag("Collectable"))
+        {
+            Destroy(other.gameObject);
+            Score(1);
+        }
     }
 }
+    
